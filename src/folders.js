6759a5a60e5e7d0a946f54b9de4eb0610c7ab1e2
Ina Davidova <ina.davidovaa@gmail.com>
2022-04-02 00:15:59 +0300

    Buttons hover effects and change of font-family

--- src/folders.js
+++ src/folders.js
@@ -1,40 +1,41 @@
 document.querySelector("main").addEventListener("click", onClick);
 let imageElement = document.querySelector("#slideshowImg");
 let slideshowDiv = document.querySelector("#slideshowDiv");
-let h2 = slideshowDiv.querySelector('h2');
+let h2 = slideshowDiv.querySelector("h2");
 
 let index = 1;
 
 function onClick(e) {
-  if (e.target.tagName == "BUTTON") {
-    if (e.target.id == "close") {
-      e.target.parentElement.parentElement.style.display = "none";
+    if (e.target.tagName == "BUTTON") {
 
-    } else if (e.target.id == "next") {
-      if (index == 10) {
-        index = 1;
-      } else {
-        index++;
-      }
+        if (e.target.id == "close") {
+            e.target.parentElement.parentElement.style.display = "none";
+        } else if (e.target.id == "next") {
+            if (index == 10) {
+                index = 1;
+            } else {
+                index++;
+            }
 
-      imageElement.src = `../images/${h2.id}/${index}.jpg`;
+            imageElement.src = `../images/${h2.id}/${index}.jpg`;
 
-    } else if (e.target.id == "previous") {
+        } else if (e.target.id == "previous") {
 
-      if (index == 1) {
-        index = 10;
-      } else {
-        index--;
-      }
+            if (index == 1) {
+                index = 10;
+            } else {
+                index--;
+            }
 
-      imageElement.src = `../images/${h2.id}/${index}.jpg`;
-
-    } else {
-      let destination = e.target.parentElement.id;
-      slideshowDiv.style.display = 'flex';
-      h2.textContent = destination.toUpperCase();
-      h2.id = destination;
-      imageElement.src = `../images/${h2.id}/${index}.jpg`;
+            imageElement.src = `../images/${h2.id}/${index}.jpg`;
+            
+        } else {
+            index = 1;
+            let destination = e.target.parentElement.id;
+            slideshowDiv.style.display = "flex";
+            h2.textContent = destination.toUpperCase();
+            h2.id = destination;
+            imageElement.src = `../images/${h2.id}/${index}.jpg`;
+        }
     }
-  }
 }
