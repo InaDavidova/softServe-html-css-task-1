4a3f800032b594c1e710191871110846e6a5e970
Ina Davidova <ina.davidovaa@gmail.com>
2022-04-01 22:49:41 +0300

    Rename of folders and slideshow functionlity

--- src/folders.js
+++ src/folders.js
@@ -1,8 +1,40 @@
-document.querySelector('main').addEventListener('click', onClick);
+document.querySelector("main").addEventListener("click", onClick);
+let imageElement = document.querySelector("#slideshowImg");
+let slideshowDiv = document.querySelector("#slideshowDiv");
+let h2 = slideshowDiv.querySelector('h2');
+
+let index = 1;
 
 function onClick(e) {
-    if(e.target.tagName == "BUTTON"){
-        const id = e.target.parentElement.id;
-        console.log(id);
+  if (e.target.tagName == "BUTTON") {
+    if (e.target.id == "close") {
+      e.target.parentElement.parentElement.style.display = "none";
+
+    } else if (e.target.id == "next") {
+      if (index == 10) {
+        index = 1;
+      } else {
+        index++;
+      }
+
+      imageElement.src = `../images/${h2.id}/${index}.jpg`;
+
+    } else if (e.target.id == "previous") {
+
+      if (index == 1) {
+        index = 10;
+      } else {
+        index--;
+      }
+
+      imageElement.src = `../images/tenerife/${index}.jpg`;
+
+    } else {
+      let destination = e.target.parentElement.id;
+      slideshowDiv.style.display = 'flex';
+      h2.textContent = destination.toUpperCase();
+      h2.id = destination;
+      imageElement.src = `../images/${h2.id}/${index}.jpg`;
     }
+  }
 }
