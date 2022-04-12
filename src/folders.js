a153887f984ee615b802dec85a87f810876139d1
Ina Davidova <ina.davidovaa@gmail.com>
2022-04-12 22:16:05 +0300

    Change photos names and logic for displaying them

--- src/folders.js
+++ src/folders.js
@@ -1,33 +1,36 @@
+import {imageData} from './imageData.js';
+
 document.querySelector("main").addEventListener("click", onClick);
 let imageElement = document.querySelector("#slideshowImg");
 let slideshowDiv = document.querySelector("#slideshowDiv");
 let h2 = slideshowDiv.querySelector("h2");
 
-let index = 1;
+let index = 0;
 
 function onClick(e) {
     if (e.target.tagName == "BUTTON") {
 
         if (e.target.id == "close") {
             e.target.parentElement.parentElement.style.display = "none";
+
         } else if (e.target.id == "next") {
-            if (index == 10) {
-                index = 1;
+            if (index == 9) {
+                index = 0;
             } else {
                 index++;
             }
 
-            imageElement.src = `../images/${h2.id}/${index}.jpg`;
+            imageElement.src = `../images/${h2.id}/${imageData[h2.id][index]}.jpg`;
 
         } else if (e.target.id == "previous") {
 
-            if (index == 1) {
-                index = 10;
+            if (index == 0) {
+                index = 9;
             } else {
                 index--;
             }
 
-            imageElement.src = `../images/${h2.id}/${index}.jpg`;
+            imageElement.src = `../images/${h2.id}/${imageData[h2.id][index]}.jpg`;
             
         } else {
             index = 1;
@@ -35,7 +38,7 @@ function onClick(e) {
             slideshowDiv.style.display = "flex";
             h2.textContent = destination.toUpperCase();
             h2.id = destination;
-            imageElement.src = `../images/${h2.id}/${index}.jpg`;
+            imageElement.src = `../images/${h2.id}/${imageData[h2.id][index]}.jpg`;
         }
     }
 }
---
+++ src/imageData.js
@@ -0,0 +1,11 @@
+export const imageData = {
+    'amsterdam': ['amsterdam_flowers', 'amsterdam-river', 'amsterdam-night-river', 'amsterdam-night', 'couple-selfie', 'pink-street', 'red-light', 'night-house', 'flowers', 'bee'],
+    'barcelona': ['familia', 'park-terasse', 'park-palms', 'guell-park', 'flamingos', 'shark', 'couple', 'cathedral', 'lizard', 'view'],
+    'chalkidiki': ['rocks-boat', 'orange-beach', 'beach-rocks', 'cross', 'beach-view', 'beach-view2', 'hammock', 'restaurant-view', 'hot-dog', 'boats'],
+    'corfu': ['ina-in-water', 'corfu-center', 'felix', 'water-shadows', 'corfu-town', 'cat', 'beach', 'sunset', 'boat-rocks', 'boats-beach'],
+    'heidelberg': ['city-view', 'city-core', 'castle-terasse', 'green-view', 'city-view2', 'couple', 'old-pharmacy', 'bridge', 'locks', 'heidelberg-square'],
+    'krushuna': ['main-waterfall', 'high-waterfall', 'couple', 'cave', 'small-waterfalls', 'devetashka-cave', 'deep-cave', 'clear-water', 'couple2', 'devetashka-cave2'],
+    'lagos': ['cave', 'canu', 'couple', 'rocks-view', 'couple2', 'friends', 'cappucino', 'alone-rock', 'waves-cave', 'higher-view'],
+    'rila': ['stones-balance', 'lakes', 'stone-balance2', 'felix', 'cable-lift', 'cable-lift-feet', 'clear-water', 'kidney-lake', 'view', 'ina'],
+    'tenerife': ['cocktails', 'sunset', 'mountain-village', 'boat-palms', 'volcano-forest', 'rocks-water', 'old-lava', 'teide-volcano', 'inner-pool', 'couple'],
+};
