ad9d477da89bebf63c8b64f97f0a20a8a5a1b40f
Ina Davidova <ina.davidovaa@gmail.com>
2022-04-24 03:18:30 +0300

    Change styles for "Dark" mode

---
+++ src/changeMode.js
@@ -0,0 +1,38 @@
+if (localStorage.getItem("dark") == "true") {
+  $("main").addClass("dark");
+  $(".mode button").addClass("dark-mode-btn");
+  $(".mode button").text("Dark");
+  $("nav a").addClass("dark-nav");
+  $("p").addClass("dark-p");
+  $("label").addClass("dark-p");
+  $(".folder").addClass("dark-folder");
+  $("#slideshowDiv").addClass("dark-slideshowDiv");
+}
+
+$(document).ready(function () {
+  $(".mode button").on("click", function (e) {
+
+    if (localStorage.getItem("dark") == "true") {
+      $("main").removeClass("dark");
+      $("nav a").removeClass("dark-nav");
+      $(".mode button").removeClass("dark-mode-btn").text("Light");
+      $("p").removeClass("dark-p");
+      $("label").removeClass("dark-p");
+      $(".folder").removeClass("dark-folder");
+      $("#slideshowDiv").removeClass("dark-slideshowDiv");
+
+      localStorage.setItem("dark", "false");
+
+    } else {
+      $("main").addClass("dark");
+      $("nav a").addClass("dark-nav");
+      $(".mode button").addClass("dark-mode-btn").text("Dark");
+      $("p").addClass("dark-p");
+      $("label").addClass("dark-p");
+      $(".folder").addClass("dark-folder");
+      $("#slideshowDiv").addClass("dark-slideshowDiv");
+
+      localStorage.setItem("dark", "true");
+    }
+  });
+});
