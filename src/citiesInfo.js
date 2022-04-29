3e05af5d7fb3975647ede77aacf2c560def9383e
Ina Davidova <ina.davidovaa@gmail.com>
2022-04-30 00:49:24 +0300

    Cities "Show More/Less" info functionallity

--- src/citiesInfo.js
+++ src/citiesInfo.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     for(let [city, info] of Object.entries(citiesData)){
         $("main").append(`
             <div class="city" id="${city}">
-            <img src="${info.picUrl}" alt="Picture of ${city}"/>    
+                <img src="${info.picUrl}" alt="Picture of ${city}"/>    
                 <div class="city-content">
                     <h2>${city}, ${info.country}</h2>
                     <p>${info.information.substring(0,150)}...</p>
@@ -15,3 +15,16 @@ $(document).ready(function () {
     }
 
 });
+
+$("main").click(function(e){
+    const element = e.target;
+
+    if(element.tagName =="BUTTON" && element.textContent =="Show More"){
+        element.textContent = "Show Less";
+        element.previousElementSibling.textContent = citiesData[element.parentElement.parentElement.id].information;
+
+    } else if(element.tagName =="BUTTON" && element.textContent =="Show Less"){
+        element.textContent = "Show More";
+        element.previousElementSibling.textContent = citiesData[element.parentElement.parentElement.id].information.substring(0, 150) + '...';
+    }
+})
