4ae6ae160f84e626a3d2d1d07c1177d73645340c
Ina Davidova <ina.davidovaa@gmail.com>
2022-05-03 22:08:11 +0300

    Adding functionality to the "like" btns for voting

--- src/vote.js
+++ src/vote.js
@@ -1,1 +1,33 @@
+$(document).ready(function () {
+    if (!localStorage.getItem("votes")) {
+        let votes = {
+            paris: 0,
+            stockholm: 0,
+            dubrovnik: 0,
+        };
 
+        localStorage.setItem("votes", JSON.stringify(votes));
+        
+    } else {
+        let votes = JSON.parse(localStorage.getItem("votes"));
+
+        $("#paris span").text(votes.paris);
+        $("#stockholm span").text(votes.stockholm);
+        $("#dubrovnik span").text(votes.dubrovnik);
+    }
+
+    $(".vote-div").on("click", function (e) {
+        const element = e.target;
+
+        if (element.tagName == "I") {
+            let votes = JSON.parse(localStorage.getItem("votes"));
+            let city = element.parentElement.parentElement.id;
+
+            votes[city]++;
+
+            element.parentElement.previousElementSibling.textContent = votes[city];
+
+            localStorage.setItem("votes", JSON.stringify(votes));
+        }
+    });
+});
